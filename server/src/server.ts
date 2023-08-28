6544de5139ed97306aa8165faddf0252217b0813
Laryssa Peçanha <laryssascampark@gmail.com>
2023-08-27 21:23:30 -0300

    Estruturação do banco de dados, tabelas relacionamentos do banco de dados e rotas do back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,12 @@
 import fastify from "fastify";
 import cors from '@fastify/cors'
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
-
-app.get('/Deus', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
