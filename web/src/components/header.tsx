2b1470869198cb690385fd6602d942eaea28371a
Laryssa Peçanha <laryssascampark@gmail.com>
2023-08-28 18:29:15 -0300

    aumento da imagem do app

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -8,7 +8,7 @@ export function Header(){
 
     return(
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-            <img src={logoImage} alt="Habits" width="20%"/>
+            <img src={logoImage} alt="Habits" width="30%"/>
             <p>Registro de Atividades</p>
 
             
